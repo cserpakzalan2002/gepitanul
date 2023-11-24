c85ae08ea4d644183e194c721a66ebe4dd214b71
cserpakzalan2002 <114362529+cserpakzalan2002@users.noreply.github.com>
2023-11-24 15:58:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ def predict_subscription_status():
     st.title("Predict Subscription Status")
 
     st.write("Please provide the following information for prediction:")
+    st.write("Note: Enter all information starting with uppercase and modify based on existing data.")
 
     age = st.number_input("Age", min_value=18, value=30)
     gender = st.radio("Gender", ('Male', 'Female'))
     location = st.text_input("Location")
     payment_method = st.text_input("Payment Method")
     frequency = st.number_input("Frequency of Purchases", min_value=0, value=5)
-    discount = st.number_input("Discount Applied", min_value=0, value=0)
+    discount = st.number_input("Discount Applied", min_value=0, value=4)
 
     ok = st.button("Predict Subscription Status")
 
-    df = pd.read_csv('shopping_trends.csv')
+    df = pd.read_csv('shopping_trends_updated.csv')
 
     label_encoders = {}
 
@@ -32,17 +33,26 @@ def predict_subscription_status():
 
     if ok:
         model.fit(X, y)
-        new_data = pd.DataFrame({
-            'Age': [age],
-            'Gender': label_encoders['Gender'].transform([gender])[0],
-            'Location': label_encoders['Location'].transform([location])[0],
-            'Payment Method': label_encoders['Payment Method'].transform([payment_method])[0],
-            'Frequency of Purchases': [frequency],
-            'Discount Applied': [discount]
-        })
+        try:
+            location = location.title() if location else ''
+            payment_method = payment_method.title() if payment_method else ''
 
-        predicted_status = model.predict(new_data)
-        status = "Subscribed" if predicted_status[0] == 1 else "Not Subscribed"
-        st.subheader(f"Estimated Subscription Status: {status}")
+            new_data = pd.DataFrame({
+                'Age': [age],
+                'Gender': label_encoders['Gender'].transform([gender])[0],
+                'Location': label_encoders['Location'].transform([location])[0],
+                'Payment Method': label_encoders['Payment Method'].transform([payment_method])[0],
+                'Frequency of Purchases': [frequency],
+                'Discount Applied': [discount]
+            })
+
+            predicted_status = model.predict(new_data)
+            status = "Subscribed" if predicted_status[0] == 1 else "Not Subscribed"
+            st.subheader(f"Estimated Subscription Status: {status}")
+        except ValueError:
+            st.write("Please enter valid data based on the provided options and start with uppercase letters.")
+            st.write("For example, if the location is 'paris', modify it to 'Paris'.")
+            st.write("Similarly, modify payment methods, if needed, to match existing records.")
+
 
 predict_subscription_status()
